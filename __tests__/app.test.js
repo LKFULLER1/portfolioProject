550ee1f6bdfc5fd3255e14013014a4f5cc229bbd
LKFULLER1 <liamfuller2000@gmail.com>
2022-11-17 14:06:31 +0000

    added category functionality and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,33 +1,33 @@
-const request = require('supertest');
-const seed = require('../db/seeds/seed')
-const testData = require('../db/data/test-data/index')
-const app = require('../app');
-const db = require('../db/connection');
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const app = require("../app");
+const db = require("../db/connection");
 
 beforeEach(() => {
-  return seed(testData)
-})
+  return seed(testData);
+});
 
 afterAll(() => {
   return db.end();
 });
 
-describe('0 - wrong endpoints', () => {
-  test('status:404, responds with a route not found error', () => {
+describe("0 - wrong endpoints", () => {
+  test("status:404, responds with a route not found error", () => {
     return request(app)
-      .get('/api/randomStuff')
+      .get("/api/randomStuff")
       .expect(404)
       .then(({ body }) => {
         const { msg } = body;
-        expect(msg).toBe('Route not found');
+        expect(msg).toBe("Route not found");
       });
   });
 });
 
-describe('3 - GET /api/categories', () => {
-  test('status:200, responds with an array of category objects', () => {
+describe("3 - GET /api/categories", () => {
+  test("status:200, responds with an array of category objects", () => {
     return request(app)
-      .get('/api/categories')
+      .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
         const { categories } = body;
@@ -36,23 +36,23 @@ describe('3 - GET /api/categories', () => {
         categories.forEach((category) => {
           expect(category).toMatchObject({
             slug: expect.any(String),
-            description: expect.any(String)
+            description: expect.any(String),
           });
         });
       });
   });
 });
 
-describe('4 - GET /api/reviews', () => {
-  test('status:200, responds with an array of review objects', () => {
+describe("4 - GET /api/reviews", () => {
+  test("status:200, responds with an array of review objects", () => {
     return request(app)
-      .get('/api/reviews')
+      .get("/api/reviews")
       .expect(200)
       .then(({ body }) => {
         const { reviews } = body;
         expect(reviews).toBeInstanceOf(Array);
         expect(reviews).toHaveLength(13);
-        expect(reviews).toBeSortedBy('created_at', {
+        expect(reviews).toBeSortedBy("created_at", {
           descending: true,
           coerce: true,
         });
@@ -67,19 +67,63 @@ describe('4 - GET /api/reviews', () => {
             review_img_url: expect.any(String),
             created_at: expect.any(String),
             votes: expect.any(Number),
-            comment_count: expect.any(Number)
+            comment_count: expect.any(Number),
           });
         });
       });
   });
 
-  describe('11-queries', () => {
-    
+  describe.only("11-queries", () => {
+    test("should return reviews of the provided category", () => {
+      return request(app)
+        .get("/api/reviews?category=dexterity")
+        .expect(200)
+        .then(({ body }) => {
+          const { reviews } = body;
+          reviews.forEach((review) => {
+            expect(review).toMatchObject({
+              category: 'dexterity'
+            });
+          });
+        });
+    });
+
+    test("should return all reviews with no category provided", () => {
+      return request(app)
+        .get("/api/reviews")
+        .expect(200)
+        .then(({ body }) => {
+          const { reviews } = body;
+          reviews.forEach((review) => {
+            expect(review).toMatchObject({
+              title: expect.any(String),
+            category: expect.any(String),
+            category: expect.any(String),
+            designer: expect.any(String),
+            owner: expect.any(String),
+            review_id: expect.any(Number),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            comment_count: expect.any(Number),
+            });
+          });
+        });
+    });
+
+    test("status:400, responds with a 400 error when the category is not valid", () => {
+      return request(app)
+        .get(`/api/reviews?category=rubbish`)
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).toBe("invalid query!");
+        });
+    });
   });
 });
 
-describe('5. GET /api/reviews/:review_id', () => {
-  test('status:200, responds with a single matching review (with comment count)', () => {
+describe("5. GET /api/reviews/:review_id", () => {
+  test("status:200, responds with a single matching review (with comment count)", () => {
     const REVIEW_ID = 2;
     return request(app)
       .get(`/api/reviews/${REVIEW_ID}`)
@@ -87,52 +131,52 @@ describe('5. GET /api/reviews/:review_id', () => {
       .then(({ body }) => {
         expect(body.review).toEqual({
           review_id: REVIEW_ID,
-          title: 'Jenga',
-          designer: 'Leslie Scott',
-          owner: 'philippaclaire9',
+          title: "Jenga",
+          designer: "Leslie Scott",
+          owner: "philippaclaire9",
           review_img_url:
-            'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-          review_body: 'Fiddly fun for all the family',
-          category: 'dexterity',
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          review_body: "Fiddly fun for all the family",
+          category: "dexterity",
           created_at: "2021-01-18T10:01:41.251Z",
           votes: 5,
-          comment_count: 3
+          comment_count: 3,
         });
       });
   });
 
-  test('status:404, responds with a 404 error when the review does not exist', () => {
+  test("status:404, responds with a 404 error when the review does not exist", () => {
     const REVIEW_ID = 200;
     return request(app)
       .get(`/api/reviews/${REVIEW_ID}`)
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe('review not found!');
+        expect(body.msg).toBe("review not found!");
       });
   });
 
-  test('status:400, responds with a 400 error when the review_id is not valid (not an integer)', () => {
-    const REVIEW_ID = 'hello';
+  test("status:400, responds with a 400 error when the review_id is not valid (not an integer)", () => {
+    const REVIEW_ID = "hello";
     return request(app)
       .get(`/api/reviews/${REVIEW_ID}`)
       .expect(400)
       .then(({ body }) => {
-        expect(body.msg).toBe('invalid query!');
+        expect(body.msg).toBe("invalid query!");
       });
   });
 });
 
-describe('6. GET /api/reviews/:review_id/comments', () => {
-  test('status:200, responds with an array of comments with the review_id', () => {
+describe("6. GET /api/reviews/:review_id/comments", () => {
+  test("status:200, responds with an array of comments with the review_id", () => {
     return request(app)
-      .get('/api/reviews/2/comments')
+      .get("/api/reviews/2/comments")
       .expect(200)
       .then(({ body }) => {
         const { comments } = body;
         expect(comments).toBeInstanceOf(Array);
         expect(comments).toHaveLength(3);
-        expect(comments).toBeSortedBy('created_at', {
-          descending: true
+        expect(comments).toBeSortedBy("created_at", {
+          descending: true,
         });
         comments.forEach((comment) => {
           expect(comment).toMatchObject({
@@ -141,47 +185,47 @@ describe('6. GET /api/reviews/:review_id/comments', () => {
             created_at: expect.any(String),
             author: expect.any(String),
             body: expect.any(String),
-            review_id: 2
+            review_id: 2,
           });
         });
       });
   });
 
-  test('status:200, responds with an empty array when review_id does not link to any comments', () => {
+  test("status:200, responds with an empty array when review_id does not link to any comments", () => {
     return request(app)
-      .get('/api/reviews/1/comments')
+      .get("/api/reviews/1/comments")
       .expect(200)
       .then(({ body }) => {
         const { comments } = body;
         expect(comments).toEqual([]);
       });
   });
-  test('status:404, responds with a 404 error when the review_id does not exist', () => {
+  test("status:404, responds with a 404 error when the review_id does not exist", () => {
     const REVIEW_ID = 200;
     return request(app)
       .get(`/api/reviews/${REVIEW_ID}/comments`)
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe('review not found!');
+        expect(body.msg).toBe("review not found!");
       });
   });
 
-  test('status:400, responds with a 400 error when the review_id is not valid (not an integer)', () => {
-    const REVIEW_ID = 'hello';
+  test("status:400, responds with a 400 error when the review_id is not valid (not an integer)", () => {
+    const REVIEW_ID = "hello";
     return request(app)
       .get(`/api/reviews/${REVIEW_ID}/comments`)
       .expect(400)
       .then(({ body }) => {
-        expect(body.msg).toBe('invalid query!');
+        expect(body.msg).toBe("invalid query!");
       });
   });
 });
 
-describe('7. POST /api/reviews/:review_id/comments', () => {
-  test('status:201, responds with comment newly added to the database', () => {
+describe("7. POST /api/reviews/:review_id/comments", () => {
+  test("status:201, responds with comment newly added to the database", () => {
     const newComment = {
-      author: 'mallionaire',
-      body: 'rubbish mate, grumble!'
+      author: "mallionaire",
+      body: "rubbish mate, grumble!",
     };
     return request(app)
       .post(`/api/reviews/2/comments`)
@@ -195,53 +239,53 @@ describe('7. POST /api/reviews/:review_id/comments', () => {
       });
   });
 
-  test('status:404, responds with a 404 error when the review_id does not exist', () => {
+  test("status:404, responds with a 404 error when the review_id does not exist", () => {
     const newComment = {
-      author: 'mallionaire',
-      body: 'rubbish mate, grumble!',
+      author: "mallionaire",
+      body: "rubbish mate, grumble!",
     };
     return request(app)
       .post(`/api/reviews/200/comments`)
       .send(newComment)
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe('review not found!');
+        expect(body.msg).toBe("review not found!");
       });
   });
 
-  test('status:400, responds with a 400 error when a (NOT NULL) property is not present - wrong format', () => {
+  test("status:400, responds with a 400 error when a (NOT NULL) property is not present - wrong format", () => {
     const newComment = {
-      body: 'rubbish mate, grumble!',
+      body: "rubbish mate, grumble!",
     };
     return request(app)
       .post(`/api/reviews/2/comments`)
       .send(newComment)
       .expect(400)
       .then(({ body }) => {
-        expect(body.msg).toBe('invalid query!');
+        expect(body.msg).toBe("invalid query!");
       });
-  })
+  });
 
-  test('should respond with a 400 error when the review_id is not valid (not an integer)', () => {
+  test("should respond with a 400 error when the review_id is not valid (not an integer)", () => {
     const newComment = {
-      author: 'mallionaire',
-      body: 'rubbish mate, grumble!',
+      author: "mallionaire",
+      body: "rubbish mate, grumble!",
     };
     return request(app)
       .post(`/api/reviews/hello/comments`)
       .send(newComment)
       .expect(400)
       .then(({ body }) => {
-        expect(body.msg).toBe('invalid query!');
-      })
+        expect(body.msg).toBe("invalid query!");
+      });
   });
-})
+});
 
-describe('8. PATCH /api/reviews/:review_id', () => {
-  test('status:202, responds with correctly updated review', () => {
+describe("8. PATCH /api/reviews/:review_id", () => {
+  test("status:202, responds with correctly updated review", () => {
     const REVIEW_ID = 3;
     const updateVotes = {
-      inc_votes: 5
+      inc_votes: 5,
     };
     return request(app)
       .patch(`/api/reviews/${REVIEW_ID}`)
@@ -250,64 +294,64 @@ describe('8. PATCH /api/reviews/:review_id', () => {
       .then(({ body }) => {
         expect(body.review).toMatchObject({
           review_id: REVIEW_ID,
-          title: 'Ultimate Werewolf',
-          category: 'social deduction',
-          designer: 'Akihisa Okui',
-          owner: 'bainesface',
+          title: "Ultimate Werewolf",
+          category: "social deduction",
+          designer: "Akihisa Okui",
+          owner: "bainesface",
           review_body: "We couldn't find the werewolf!",
-          review_img_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-          created_at: '2021-01-18T10:01:41.251Z',
-          votes: 10
+          review_img_url:
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          created_at: "2021-01-18T10:01:41.251Z",
+          votes: 10,
         });
       });
   });
 
-  test('status:404, responds with a 404 error when the review_id does not exist', () => {
+  test("status:404, responds with a 404 error when the review_id does not exist", () => {
     const REVIEW_ID = 200;
     const updateVotes = {
-      inc_votes: 5
+      inc_votes: 5,
     };
     return request(app)
       .patch(`/api/reviews/${REVIEW_ID}`)
       .send(updateVotes)
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe('review not found!');
+        expect(body.msg).toBe("review not found!");
       });
   });
 
-  test('status:400, responds with a 400 error when a (NOT NULL) property is not present - wrong format', () => {
+  test("status:400, responds with a 400 error when a (NOT NULL) property is not present - wrong format", () => {
     const REVIEW_ID = 2;
+    const updateVotes = {};
+    return request(app)
+      .patch(`/api/reviews/${REVIEW_ID}`)
+      .send(updateVotes)
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("invalid query!");
+      });
+  });
+
+  test("should respond with a 400 error when the review_id is not valid (not an integer)", () => {
+    const REVIEW_ID = "hello";
     const updateVotes = {
+      inc_votes: 5,
     };
     return request(app)
       .patch(`/api/reviews/${REVIEW_ID}`)
       .send(updateVotes)
       .expect(400)
       .then(({ body }) => {
-        expect(body.msg).toBe('invalid query!');
+        expect(body.msg).toBe("invalid query!");
       });
-  })
-
-  test('should respond with a 400 error when the review_id is not valid (not an integer)', () => {
-    const REVIEW_ID = 'hello';
-    const updateVotes = {
-      inc_votes: 5
-    };
-    return request(app)
-    .patch(`/api/reviews/${REVIEW_ID}`)
-    .send(updateVotes)
-    .expect(400)
-    .then(({ body }) => {
-      expect(body.msg).toBe('invalid query!');
-    });
   });
 });
 
-describe('9 - GET /api/users', () => {
-  test('status:200, responds with an array of user objects', () => {
+describe("9 - GET /api/users", () => {
+  test("status:200, responds with an array of user objects", () => {
     return request(app)
-      .get('/api/users')
+      .get("/api/users")
       .expect(200)
       .then(({ body }) => {
         const { users } = body;
@@ -317,10 +361,9 @@ describe('9 - GET /api/users', () => {
           expect(user).toMatchObject({
             username: expect.any(String),
             name: expect.any(String),
-            avatar_url: expect.any(String)
+            avatar_url: expect.any(String),
           });
         });
       });
   });
 });
-
--- models/games.js
+++ models/games.js
@@ -1,14 +1,31 @@
-const db = require('../db/connection.js');
+const db = require("../db/connection.js");
+const reviews = require("../db/data/test-data/reviews.js");
 
 exports.selectCategories = () => {
-    return db.query('SELECT * FROM categories;')
-        .then(data => {
-            return data.rows;
-        })
+  return db.query("SELECT * FROM categories;").then((data) => {
+    return data.rows;
+  });
 };
 
-exports.selectReviews = () => {
-    return db.query(`SELECT owner,
+exports.selectReviews = (category = 'reviews.category', sort_by, order_by) => {
+  console.log(category);
+  console.log(sort_by);
+  console.log(order_by);
+  const validCategories = [
+    "euro game",
+    "social deduction",
+    "dexterity",
+    "children's game",
+    "reviews.category"
+  ];
+  if ((!validCategories.includes(category))){
+    console.log('rejecting');
+    return Promise.reject({ status: 400, msg: "invalid query!" });
+  }
+
+  return db
+    .query(
+      `SELECT owner,
         title, 
         reviews.review_id,
         category,
@@ -20,67 +37,82 @@ exports.selectReviews = () => {
         FROM reviews 
     FULL OUTER JOIN comments
         ON reviews.review_id = comments.review_id
+        WHERE reviews.category = '${category}' 
         GROUP BY reviews.review_id
-        ORDER BY reviews.created_at DESC;`)
-        .then(data => {
-            return data.rows;
-        })
+        ORDER BY reviews.created_at DESC;`
+    )
+    .then((data) => {
+      return data.rows;
+    });
 };
 
 exports.selectReviewById = (Review_id) => {
-    return db.query(`SELECT reviews.*, 
+  return db
+    .query(
+      `SELECT reviews.*, 
     CAST(COUNT(*)AS int) AS comment_count 
     FROM reviews 
     FULL OUTER JOIN comments
     ON reviews.review_id = comments.review_id
     WHERE reviews.review_id = $1
-    GROUP BY reviews.review_id;`, [Review_id])
-        .then(data => {
-            if (data.rows.length === 0) {
-                //util function to check if something exists - reject if not
-                return Promise.reject({ status: 404, msg: 'review not found!' });
-            }
-            return data.rows[0];
-        })
+    GROUP BY reviews.review_id;`,
+      [Review_id]
+    )
+    .then((data) => {
+      if (data.rows.length === 0) {
+        //util function to check if something exists - reject if not
+        return Promise.reject({ status: 404, msg: "review not found!" });
+      }
+      return data.rows[0];
+    });
 };
 
 exports.selectCommentsByReviewId = (review_id) => {
-    return this.selectReviewById(review_id).then(result => {
-        return db.query(`SELECT * FROM comments WHERE review_id = $1 ORDER BY created_at DESC;`, [review_id])
-            .then(data => {
-                return data.rows;
-            })
-    })
+  return this.selectReviewById(review_id).then((result) => {
+    return db
+      .query(
+        `SELECT * FROM comments WHERE review_id = $1 ORDER BY created_at DESC;`,
+        [review_id]
+      )
+      .then((data) => {
+        return data.rows;
+      });
+  });
 };
 
 exports.insertCommentByReviewId = (newComment, review_id) => {
-    return this.selectReviewById(review_id).then(() => {
-        return db.query(`INSERT INTO comments
+  return this.selectReviewById(review_id).then(() => {
+    return db
+      .query(
+        `INSERT INTO comments
         (author, body, review_id)
         VALUES ($1, $2, $3)
         RETURNING *;`,
-            [newComment.author, newComment.body, review_id])
-            .then((resultOfInsert) => {
-                return resultOfInsert.rows[0];
-            })
-    })
+        [newComment.author, newComment.body, review_id]
+      )
+      .then((resultOfInsert) => {
+        return resultOfInsert.rows[0];
+      });
+  });
 };
 
 exports.updateReview = (updateVotes, review_id) => {
-        return db.query(`UPDATE reviews SET votes = votes + ${updateVotes.inc_votes} 
+  return db
+    .query(
+      `UPDATE reviews SET votes = votes + ${updateVotes.inc_votes} 
         WHERE review_id = ${review_id}
-        RETURNING *;`)
-        .then((resultOfPatch) => {
-            if (resultOfPatch.rows.length === 0){
-                return Promise.reject({ status: 404, msg: 'review not found!' });
-            }
-            return resultOfPatch.rows[0];
-        })
+        RETURNING *;`
+    )
+    .then((resultOfPatch) => {
+      if (resultOfPatch.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "review not found!" });
+      }
+      return resultOfPatch.rows[0];
+    });
 };
 
 exports.selectUsers = () => {
-    return db.query(`SELECT * FROM users;`)
-    .then(data => {
-        return data.rows;
-    })
-}
+  return db.query(`SELECT * FROM users;`).then((data) => {
+    return data.rows;
+  });
+};
